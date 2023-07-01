52c86d820309097e7a7aa948f1869a3ce2bd998d
Jean-Dv <mrjunior127@gmail.com>
2023-07-01 11:59:16 -0500

    docs: added documentation for the class Roman

--- src/domain/Roman.java
+++ src/domain/Roman.java
@@ -2,7 +2,14 @@ package domain;
 
 import java.util.Map;
 
+/**
+ * The Roman class provides methods to convert a roman number to an arabic
+ * number.
+ */
 public class Roman {
+    /**
+     * Mapping of roman characters to their equivalent arabic numbers.
+     */
     private static final Map<Character, Integer> ROMAN_TO_ARABIC = Map.of(
             'I', 1,
             'V', 5,
@@ -12,10 +19,22 @@ public class Roman {
             'D', 500,
             'M', 1000);
 
+    /**
+     * Checks if a given string represents a valid roman numeral.
+     * 
+     * @param roman The string to be validated as a roman numeral.
+     * @return true if the string is a valid roman numeral, false otherwise.
+     */
     private static boolean isValid(String roman) {
         return roman.matches("^[IVXLCDM]+$");
     }
 
+    /**
+     * Checks if the order of roman characters is valid.
+     * 
+     * @param romanChars An array of strings representing the roman characters.
+     * @return true if the order of characters is valid, false otherwise.
+     */
     private static boolean isValidOrder(String[] romanChars) {
         for (int i = 0; i < romanChars.length - 1; i++) {
             int current = ROMAN_TO_ARABIC.get(romanChars[i].charAt(0));
@@ -29,22 +48,37 @@ public class Roman {
         return true;
     }
 
-    private static boolean ensureCharactersNotRepeat(String[] roman) {
-        for (int i = 0; i < roman.length - 1; i++) {
-            int next = i + 1 < roman.length ? i + 1 : 0;
-            if (roman[i].equals("D") && roman[i].equals(roman[next])) {
+    /**
+     * Ensures that roman characters are not repeated more than 3 times, with
+     * characters specified L, D, V.
+     * 
+     * @param romanChars An array of string representing the roman characters.
+     * @return true if the characters are not repeated more than 3 times, false
+     *         otherwise.
+     */
+    private static boolean ensureCharactersNotRepeat(String[] romanChars) {
+        for (int i = 0; i < romanChars.length - 1; i++) {
+            int next = i + 1 < romanChars.length ? i + 1 : 0;
+            if (romanChars[i].equals("D") && romanChars[i].equals(romanChars[next])) {
                 return false;
             }
-            if (roman[i].equals("L") && roman[i].equals(roman[next])) {
+            if (romanChars[i].equals("L") && romanChars[i].equals(romanChars[next])) {
                 return false;
             }
-            if (roman[i].equals("V") && roman[i].equals(roman[next])) {
+            if (romanChars[i].equals("V") && romanChars[i].equals(romanChars[next])) {
                 return false;
             }
         }
         return true;
     }
 
+    /**
+     * Converts a roman numberal to its equivalent arabic number.
+     * 
+     * @param roman The roman numeral to be converted.
+     * @return The equivalent arabic number, or null if the roman numeral is not
+     *         valid.
+     */
     public static Integer convert(String roman) {
         String[] romanChars = roman.split("");
         if (!isValid(roman)) {
