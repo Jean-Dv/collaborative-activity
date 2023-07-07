cddbea574bdff6c961c381bcfec33367cc066745
Daniela Guevara <danielaguevara09@outlook.com>
2023-07-06 19:27:54 -0500

    Creation of the menu and exceptions

---
+++ src/domain/Menu.java
@@ -0,0 +1,31 @@
+package domain;
+
+/**
+ * The Menu class provides methods for interacting with the user.
+ */
+public class Menu {
+    
+    public static void showMenu (){
+
+        String menu = "------------- MAIN MENU -------------/n/n" + 
+                "-------- Type the desired option --------" +
+                "1. Roman numerals" +
+                "2. Prime factors" +
+                "3. Delete spaces" +
+                "4. Egolatras numbers" +
+                "5. Magic numbers" +
+                "6. Dates" +
+                "7. Exit";        
+        
+    }
+
+    public static String validateRoman(){
+
+        String roman = "";
+
+        return roman;
+    }
+    
+
+
+}
