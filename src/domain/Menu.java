62684a8e69f9d1d76681d5580789f8f8ccaccc25
Daniela Guevara <danielaguevara09@outlook.com>
2023-07-07 17:18:15 -0500

    docs: added documentation for the Menu class

--- src/domain/Menu.java
+++ src/domain/Menu.java
@@ -1,12 +1,14 @@
 package domain;
 
+import java.text.DateFormat;
+
 import javax.swing.JOptionPane;
 
 /**
  * The Menu class provides methods for interacting with the user.
  */
 public class Menu {
-
+    
     public static void main (String []args){
         showMenu();
     }
@@ -64,23 +66,36 @@ public class Menu {
     
     }
 
+    /**
+     * Requests a Roman numeral from the user and displays the number in its decimal value.
+     */
     public static void roman(){
         
         String roman = JOptionPane.showInputDialog("Enter a roman number");
+        roman = roman.toUpperCase();
+        JOptionPane.showMessageDialog(null, Roman.convert(roman));
 
-        if (Roman.convert(roman) == null) {
-            JOptionPane.showMessageDialog(null, "Null");
-        }   
     }
 
+    
     public static void cousings(){
         
     }
 
+    /**
+     * Prompts the user for a string and returns the string without spaces.
+     */
     public static void spaces(){
+        /* 
+        String text = JOptionPane.showInputDialog("Enter a text string");
+        JOptionPane.showMessageDialog(null, RemoveExtraSpaces.remove(text));
+        */
 
     }
 
+    /**
+     * Prompts the user for a number and checks if the number is egolatrous.
+     */
     public static void egolatrous(){
 
         String numberString = JOptionPane.showInputDialog("Enter a number");
@@ -104,14 +119,25 @@ public class Menu {
 
     }
 
+    /**
+     * Prompts the user for a number and checks if the number is magic.
+     */
     public static void magic(){
 
     }
 
+    /**
+     * Receives a date in a specific format and displays a description of the date.
+     */
     public static void dates(){
-
+        /*
+        String date = JOptionPane.showInputDialog("Enter a date with the following format: dd/mm/aaaa");
+        */
     }
 
+    /**
+     * Close menu.
+     */
     public static void exit(){
         int option = JOptionPane.showConfirmDialog(null,"Are you sure?","Exit",JOptionPane.YES_NO_OPTION);
         if( option == JOptionPane.YES_OPTION ){
