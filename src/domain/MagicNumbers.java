c137ea0753785d48f2d84cd232f94f506ef03694
vvg1058 <valen9.vvg@gmail.com>
2023-07-08 15:14:16 -0500

    feat: add the MagicNumbers Class

---
+++ src/domain/MagicNumbers.java
@@ -0,0 +1,36 @@
+package domain;
+import java.util.Arrays;
+public class MagicNumbers {
+/* The class provides a method to verify if a number is magic.
+ * @param num is the number to verify
+ * @return true is the number is 'magic' , false otherwise.
+ */
+
+    public static boolean isMagicNumber(int num){
+            String numString = String.valueOf(num);
+            char[] digits = numString.toCharArray();
+            boolean magic;
+
+            Arrays.sort(digits);
+            String ascendingString = new String(digits);
+            String descendingString = new StringBuilder(ascendingString).reverse().toString();
+    
+            int ascendingNumber = Integer.parseInt(ascendingString);
+            int descendingNumber = Integer.parseInt(descendingString);
+    
+            int res = descendingNumber - ascendingNumber;
+            if(res==num){
+                 magic= true;
+                 return magic;
+            }
+            else{
+                magic=false;
+            
+            return magic;
+            }
+    
+            
+    }
+    
+    
+}
