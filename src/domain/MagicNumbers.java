bd8978740c0c9bd382bc234b420d9e5eb4c92f60
vvg1058 <valen9.vvg@gmail.com>
2023-07-08 16:09:37 -0500

    fix: fix the documentation and the return

--- src/domain/MagicNumbers.java
+++ src/domain/MagicNumbers.java
@@ -1,36 +1,34 @@
 package domain;
+
 import java.util.Arrays;
+
+/* 
+* The class provides a method to verify if a number is magic.
+*/
 public class MagicNumbers {
-/* The class provides a method to verify if a number is magic.
- * @param num is the number to verify
- * @return true is the number is 'magic' , false otherwise.
- */
 
-    public static boolean isMagicNumber(int num){
-            String numString = String.valueOf(num);
-            char[] digits = numString.toCharArray();
-            boolean magic;
+    /*
+     * The function verify if a number is magic.
+     * 
+     * @param num is the number to verify
+     * @return true is the number is 'magic' , false otherwise.
+     */
+    public static boolean isMagicNumber(int num) {
+        String numString = String.valueOf(num);
+        char[] digits = numString.toCharArray();
 
-            Arrays.sort(digits);
-            String ascendingString = new String(digits);
-            String descendingString = new StringBuilder(ascendingString).reverse().toString();
-    
-            int ascendingNumber = Integer.parseInt(ascendingString);
-            int descendingNumber = Integer.parseInt(descendingString);
-    
-            int res = descendingNumber - ascendingNumber;
-            if(res==num){
-                 magic= true;
-                 return magic;
-            }
-            else{
-                magic=false;
-            
-            return magic;
-            }
-    
-            
+        Arrays.sort(digits);
+        String ascendingString = new String(digits);
+        String descendingString = new StringBuilder(ascendingString).reverse().toString();
+
+        int ascendingNumber = Integer.parseInt(ascendingString);
+        int descendingNumber = Integer.parseInt(descendingString);
+
+        int res = descendingNumber - ascendingNumber;
+        if (res == num) {
+            return true;
+        }
+        return false;
     }
-    
-    
+
 }
